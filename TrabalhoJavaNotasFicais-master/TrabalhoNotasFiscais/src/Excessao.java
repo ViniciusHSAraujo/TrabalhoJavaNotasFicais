05c807d7e79fbca3d1b0bf35ce53642e3abae898
ViniciusHSAraujo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2019-05-02 16:01:45 -0300

    Revert "Merge branch 'master' of https://github.com/ViniciusHSAraujo/TrabalhoJavaNotasFicais"
    This reverts commit 346bf137143a19f553353a96ddb6593dd573c5b6, reversing
    changes made to 967274c706da6d04d3244e4bf030a47187b1e461.

--- TrabalhoJavaNotasFicais-master/TrabalhoNotasFiscais/src/Excessao.java
+++
@@ -1,9 +0,0 @@
-
-public class Excessao extends Exception {
-
-	private static final long serialVersionUID = -5977373508888201087L;
-
-	public Excessao(String msg) {
-		super("Erro na operação: " + msg);
-	}
-}
